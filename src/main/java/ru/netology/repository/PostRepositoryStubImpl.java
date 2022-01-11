98aa28aa225b77b00d6cc1dd9e0c067118f57a51
Vitaly <supercyfqgth2010@yandex.ru>
2022-01-11 20:40:01 +0300

    added homeworkSpringWVC

---
+++ src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -0,0 +1,47 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepositoryStubImpl implements PostRepository {
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong count = new AtomicLong();
+    public List<Post> all() {
+        List<Post> postList = new ArrayList<>();
+        for (Map.Entry<Long, Post> posts : posts.entrySet()) {
+            postList.add(posts.getValue());
+        }
+        return postList;
+    }
+
+    public Optional<Post> getById(long id) {
+        if (posts.containsKey(id)) return Optional.of(posts.get(id));
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(count.incrementAndGet());
+            posts.put(post.getId(), post);
+        } else if (post.getId() != 0) {
+            if (posts.containsKey(post.getId())) {
+                posts.replace(post.getId(), post);
+            } else {
+                throw new NotFoundException();
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+        } else throw new NotFoundException();
+    }
+}
